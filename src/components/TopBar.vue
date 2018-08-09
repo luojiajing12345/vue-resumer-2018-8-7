36b0169144703c635f4244b05d70a06fcf059dbc
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2018-08-09 15:49:36 +0800

    添加eleme组件

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,13 +1,25 @@
 <template>
     <div id='topbar'>
-        我是'TopBar'
+        <div class='logo'>
+            VueResumer
+        </div>
+        <div class="actions">
+            <button>注册</button>
+            <button>登陆</button>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </div>
     </div>
 </template>
-<style>
+<style lang='scss'>
     #topbar{
-        border:1px solid green;
-        min-height:100px;
-
+        display: flex;
+        justify-content: space-between;
+        padding: 20px
     }
 
 </style>
