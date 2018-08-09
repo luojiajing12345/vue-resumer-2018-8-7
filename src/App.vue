26acf153db0c42fc0cd69aebb3badec40088941e
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2018-08-09 18:47:13 +0800

    添加图标

--- src/App.vue
+++ src/App.vue
@@ -10,38 +10,58 @@
 </template>
 
 <script>
-
 //import frank from './components/frank'
-import TopBar from './components/TopBar'
-import PreView from './components/PreView'
-import EdiTor from './components/EdiTor'
+import TopBar from "./components/TopBar";
+import PreView from "./components/PreView";
+import EdiTor from "./components/EdiTor";
 export default {
   components: {
-    TopBar,PreView,EdiTor
+    TopBar,
+    PreView,
+    EdiTor
   }
-}
+};
 </script>
 
 <style lang='scss'>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  height:100vh;
-  display:flex;
-  flex-direction:column;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
-.topbar{
+.topbar {
+  box-shadow: 0 0 3px hsla(0, 0, 0, 0.5);
+  position: relative;
+  z-index: 1;
 }
-main{
-  display:flex;
-  flex:1;
-  > .editor{
-  width:20em;
-  }
-  > .preview{
-  flex:1;
-  }
-}
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 
+}
+main {
+  display: flex;
+  flex: 1;
+  background: #ddd;
+  > .editor {
+    width: 40em;
+    margin: 20px 10px 20px 10px;
+    background: white;
+    box-shadow: 0 0 3px hsla(0, 0, 0, 0.5);
+    border-radius: 10px;
+  }
+  > .preview {
+    flex: 1;
+    margin: 20px 20px 20px 10px;
+    background: white;
+    box-shadow: 0 0 3px hsla(0, 0, 0, 0.5);
+    border-radius: 10px;
+  }
+}
 </style>
--- src/components/EdiTor.vue
+++ src/components/EdiTor.vue
@@ -1,11 +1,52 @@
 <template>
     <div id='editor'>
-        我是'editor'
+        <nav>
+            <ol>
+                <li>
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-shenfen"></use>
+                    </svg>
+                </li>
+                <li>
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-book"></use>
+                    </svg>
+                </li>
+                <li>
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-work0"></use>
+                    </svg>
+                </li>
+                <li>
+                        <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-aixin"></use>
+                    </svg>
+                </li>
+                <li>
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-jiangbei"></use>
+                    </svg>
+                </li>
+                <li>
+                    <svg class="icon" aria-hidden="true">
+                        <use xlink:href="#icon-dianhua"></use>
+                    </svg>
+                </li>
+            </ol>
+        </nav>
+        <ol class="panes"></ol>
     </div>
 </template>
 <style>
-    #editor{
-        border:1px solid black;
-          min-height:100px;
-    }
+nav{
+    background: black;
+}
+#editor {
+  min-height: 100px;
+}
+.icon{
+     width: 30px;
+     height: 30px;
+     fill:white;
+}
 </style>
